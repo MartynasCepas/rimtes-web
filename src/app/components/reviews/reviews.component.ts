115e0122f277f669e42392459acc3e1071db0946
Martynas Čepas <martynas.cepas1@gmail.com>
2023-01-28 17:53:58 +0200

    style changes

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,20 +1,21 @@
 import { Review } from './../../models/review';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterContentChecked, AfterViewChecked, Component, OnInit, ViewChild } from '@angular/core';
 import ReviewsJson from '../../../assets/json/reviews.json';
 import { Carousel } from 'primeng/carousel';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.scss']
 })
-export class ReviewsComponent implements OnInit {
+export class ReviewsComponent implements OnInit, AfterViewChecked {
     reviews: Review[] = [];
     responsiveOptions: any[] = [];
 
-    @ViewChild('carousel') carousel: Carousel;
+    @ViewChild(Carousel) carousel: Carousel;
 
-    constructor() {
+    constructor(private renderer: Renderer2) {
       this.reviews = ReviewsJson.reviews;
 
       this.responsiveOptions = [
@@ -31,8 +32,32 @@ export class ReviewsComponent implements OnInit {
     ];
     }
 
+  ngAfterViewChecked(): void {
+    if(this.carousel?.el?.nativeElement) {
+      const itemsArray = this.carousel.el.nativeElement.querySelectorAll('.p-carousel-item-active');
+      if(this.carousel.numVisible === 3) {
+        itemsArray.forEach(item => {
+          this.renderer.removeClass(item, 'middle-carousel-item');
+        })
+        let itemId = 1;
+        if(this.carousel.page === 2){
+          itemId = 4;
+        }
+        const item = itemsArray[itemId];
+        item.classList.add('middle-carousel-item');
+      }
+      else if (this.carousel.numVisible === 1) {
+        itemsArray.forEach(item => {
+          if(!item.classList.contains('middle-carousel-item'))
+            this.renderer.addClass(item, 'middle-carousel-item');
+        })
+      }
+    }
+  }
+
   ngOnInit(): void {
     console.log(this.carousel)
   }
+
 
 }
